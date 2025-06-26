570a1b467d61122d9bb932b0f6a35638f22d0bb5
yusufsyahputraa <2311102441213@umkt.ac.id>
2025-06-27 07:24:16 +0800

    Fix database config and Vercel deploy setup

--- myproject/settings.py
+++ myproject/settings.py
@@ -102,7 +102,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
