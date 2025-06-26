dcae15c7d1fbd373abd21e74e01861ea0c1321d8
yusufsyahputraa <2311102441213@umkt.ac.id>
2025-06-27 04:58:26 +0800

    Update DATABASES config for Railway

--- myproject/settings.py
+++ myproject/settings.py
@@ -99,11 +99,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),  # dari Railway
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
