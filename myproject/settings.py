4cead9ff54927d1012f5a96be3646b5a2fed0a92
yusufsyahputraa <2311102441213@umkt.ac.id>
2025-06-27 13:48:56 +0800

    Fix STATIC_ROOT and collectstatic for Vercel

--- myproject/settings.py
+++ myproject/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = (BASE_DIR / 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 
 
