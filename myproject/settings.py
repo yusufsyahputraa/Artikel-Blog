86fefc09f651cbdd2712af7a7347865c6c66d80e
yusufsyahputraa <2311102441213@umkt.ac.id>
2025-06-27 10:15:05 +0800

    commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -142,10 +142,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATICFILES_DIRS = [str(BASE_DIR / 'accounts' / 'static')]
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+
 
 
 # Media files
