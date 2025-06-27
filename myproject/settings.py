87bf4640551f5e935efd2f8f31dad15f53ff61aa
yusufsyahputraa <2311102441213@umkt.ac.id>
2025-06-27 16:31:46 +0800

    Update vercel.json dan static settings untuk deploy

--- myproject/settings.py
+++ myproject/settings.py
@@ -157,9 +157,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
