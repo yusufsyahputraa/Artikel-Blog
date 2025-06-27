b914b06bc481fd2fdf1230ac5e485ebab69a04a5
yusufsyahputraa <2311102441213@umkt.ac.id>
2025-06-27 13:40:39 +0800

    commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -144,8 +144,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR / 'accounts' / 'static')]
-STATIC_ROOT = str(BASE_DIR / 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = (BASE_DIR / 'staticfiles')
 
 
 
